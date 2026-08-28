ea68fcd85433a32d8a04e7f0ad5424e65675be49
Mitch Smith <37649155+mitchbne@users.noreply.github.com>
2026-08-28 15:31:44 +1000

    Show API token in CLI help

--- internal/cli/root_test.go
+++ internal/cli/root_test.go
@@ -1,10 +1,31 @@
 package cli
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/alecthomas/kong"
 )
+
+func TestQueueHelpIncludesAPIToken(t *testing.T) {
+	var root rootCommand
+	var output bytes.Buffer
+	parser, err := kong.New(&root, kong.Writers(&output, &output))
+	if err != nil {
+		t.Fatal(err)
+	}
+	context, err := kong.Trace(parser, []string{"queue"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := context.PrintUsage(false); err != nil {
+		t.Fatal(err)
+	}
+	if !strings.Contains(output.String(), "--api-token=STRING") {
+		t.Fatalf("queue help does not include --api-token:\n%s", output.String())
+	}
+}
 
 func TestEndpointDefaultsToRESTAPI(t *testing.T) {
 	var root rootCommand
